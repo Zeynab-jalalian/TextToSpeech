71bc412817fd71e5486ddc35b85a9874f23fc556
zeynab jalalian <zjalalianweb@gmail.com>
2025-02-03 15:14:38 +0330

    js 1

---
+++ script.js
@@ -0,0 +1,29 @@
+const text=document.querySelector("textarea");
+const button=document.querySelector("button");
+let isSpeaking=true;
+
+
+
+const convert=()=>{
+   
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+button.addEventListener("click",convert);
