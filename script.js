3d1d2c58c4bdeaf8e31fa7ec2fc70901870524cf
zeynab jalalian <zjalalianweb@gmail.com>
2025-02-03 15:56:45 +0330

    fix

--- script.js
+++ script.js
@@ -1,56 +1,36 @@
 const textarea=document.querySelector("textarea");
 const button=document.querySelector("button");
-let rts=true;
-//اگر صحیح باشد یعنی آماده ی  پخش صدا است
+let isSpeaking=true;
 
-// اگر غلط باشد یعنی اماده ی قطع شدن است
-const convert=()=>{
-   const synth=window.speechSynthesis;
-   const text=textarea.value;
-   if(!synth.speaking  && text){
-   const convert=new SpeechSynthesisUtterance(text);
-   synth.speak(convert);
-   }
-   if(text.length>50){
-      if(synth.speaking && rts ){
-         button.innerHTML="Pause";
-         synth.resume();
-         rts=false;
-      }else{
-         button.innerHTML="Resume";
-         synth.pause();
-         rts=true;
-      }
-   }
+const textToSpeech=()=>{
+    const synth=window.speechSynthesis;
+    const text=textarea.value;
 
-   else{
-      rts=false;
-      button.innerHTML="Speaking";
-   }
-   setInterval(()=>{
-      if(!synth.speaking && !rts){
-         rts=true;
-         button.innerHTML="Convert to Speech";
-      }
-   })
+ if(!synth.speaking && text){
+    const utternace=new SpeechSynthesisUtterance(text);
+    synth.speak(utternace);
+ }
+ if(text.length>50){
+    if(synth.speaking && isSpeaking){
+        button.innerHTML="Pause";
+        synth.resume();
+        isSpeaking=false;
+    }else{
+        button.innerHTML="Resume";
+        synth.pause();
+        isSpeaking=true;
+    }
+    
+ }else{
+    isSpeaking=false;
+    button.innerHTML="Speaking";
+ }
+
+ setInterval(()=>{
+    if(!synth.speaking && !isSpeaking){
+        isSpeaking=true;
+        button.innerHTML="Convert to Speech";
+    }
+ })
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-button.addEventListener("click",convert);
+button.addEventListener("click",textToSpeech);
